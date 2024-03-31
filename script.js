5cea713f3286c451fd26beb55c022a1aef602e57
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-01 04:05:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+
+let submitBtn = document.getElementById("submit");
+function submitFormAndAlert(e){
+	e.preventDefault;
+	let firstName = document.getElementById("first_name").value;
+let lastName = document.getElementById("last_name").value;
+let phneNumber = document.getElementById("phone_number").value;
+let emailId = document.getElementById("email_id").value;
+	let message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phneNumber} Email ID: ${emailId}`
+	alert(message)
+}
+
+submitBtn.addEventListener("click",submitFormAndAlert)
